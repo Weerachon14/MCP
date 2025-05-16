8b4a78ce3cb00bc917f3053127e20f59bf9e1aad
Weerachon14 <weerachon.sun14@gmail.com>
2025-05-16 11:53:46 +0700

    add function write and read

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ from typing import Any
 import httpx
 from mcp.server.fastmcp import FastMCP
 import json
+import os
+
 # Create an MCP server
 mcp = FastMCP("curialab")
 
 mock_api_url = "https://68258baf0f0188d7e72d597b.mockapi.io/curia/api/votes"
+
+NOTE_FILE = os.path.join(os.path.dirname(__file__),"note.txt")
+
+def ensure_file():
+    if not os.path.exists(NOTE_FILE):
+        with open(NOTE_FILE,"w") as f:
+            f.write("")
 
 # Add an addition tool
 @mcp.tool()
@@ -28,7 +37,22 @@ def search_votes(query: str):
 def add_votes(name:str, vp:float):
     response = httpx.post(mock_api_url, json= {"name": name, "vp": vp})
     return response.json()
-    
+
+@mcp.tool()
+def write_note(message:str) -> str:
+    ensure_file()
+    with open(NOTE_FILE,"a") as f:
+        f.write(message + "\n")
+    return "Save note!"
+
+@mcp.tool()
+def read_note() -> str:
+    """Read and return the content of the saved note"""
+    ensure_file()
+    with open(NOTE_FILE, "r") as f:
+        content = f.read()
+    return content if content.strip() else "Note is empty."
+
 if __name__ == "__main__":
     # Initialize and run the server
     mcp.run(transport='stdio')
